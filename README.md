325969cb80c9b855fac2215ba601896d372c7333
allactah-Desktop <andy.llactahuamani@ryerson.ca>
2022-03-21 17:55:20 -0400

    Added README and comments

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Title
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+Script that generates a number of spheres and measures the distance to make sure that there are no overlapping spheres
+
+## Getting Started <a name = "getting_started"></a>
+
+Copy and paste code into script editor or place file into script folder and import in editor
+
+## Usage <a name = "usage"></a>
+
+It takes a minute to iterate through the spheres
--- scatteredSpheres.py
+++ scatteredSpheres.py
@@ -1,45 +1,61 @@
 from maya import cmds
 import random
 
+# generates the coordinates of the first Sphere
 firstSphereX = random.uniform(-12,12)
 firstSphereZ = random.uniform(-12,12)
 
+# creates and moves the sphere in position
 cmds.polySphere(r=0.5, name='pSphere0')
 cmds.move(firstSphereX, 0, firstSphereZ)
 
+# creates a list and adds the first sphere
 existingSpheres = ['pSphere0']
 
+# creates a distanceBetween node
 cmds.shadingNode('distanceBetween', asUtility=True)
 
+# creates number of spheres
 for i in range(500):
+	#  generates new coordinates for new sphere
 	testSphereX = random.uniform(-12,12)
 	testSphereZ = random.uniform(-12,12)
 	
+	# creates and moves the new sphere in position
 	cmds.polySphere(r=0.5, name='testSphere')
 	cmds.move(testSphereX, 0, testSphereZ)
 	
+	# number of spheres added to the list
 	numSpheres = len(existingSpheres)
 	# print(f'numSpheres:{numSpheres})')
 	
 	counter = 0
 	
+	# loop through the list of existing spheres
 	for j in range(numSpheres):
 		# print(f'j: {j}')
+		# connects the distanceBetween node to the test sphere
 		cmds.connectAttr('testSphere.translate', 'distanceBetween1.point1')
+		# connects the other end of the distanceBetweenNode to the current existing sphere
 		cmds.connectAttr(f'{existingSpheres[j]}.translate', 'distanceBetween1.point2')
 		
+		# get the distance between the two
 		dist = cmds.getAttr('distanceBetween1.distance')
 		
+		# disconnect distance betweenNode
 		cmds.disconnectAttr('testSphere.translate','distanceBetween1.point1')
 		cmds.disconnectAttr(f'{existingSpheres[j]}.translate', 'distanceBetween1.point2')
 		
+		# if the distance is more than 1 then increment the counter
 		if dist > 1:
 			counter = counter + 1
+
+	# if the counter is equal to the length of the list
 	if counter == len(existingSpheres):
-		# rename the testSphere
+		# rename the testSphere and append it to the list
 		cmds.rename('testSphere', f'pSphere{str(len(existingSpheres))}')
 		existingSpheres.append(f'pSphere{str(len(existingSpheres))}')
-		# add it to the list of existing spheres
 	else:
+		# otherwise delete the testSphere
 		cmds.select('testSphere', replace=True)
 		cmds.delete()
